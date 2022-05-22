510e4bed38a0439a3b17f87e8295bbab9f5d72b4
debysdm <debysdm@hotmail.com>
2022-05-22 20:05:15 -0300

    Atualição do Modulo II

---
+++ Bootcamps/GFT Start Woman Java/Modulo II/primeiro programa java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// Primeiro programa para conhecer IDEs
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello Word!" + (a+b));*/
+
+	}
+
+}
