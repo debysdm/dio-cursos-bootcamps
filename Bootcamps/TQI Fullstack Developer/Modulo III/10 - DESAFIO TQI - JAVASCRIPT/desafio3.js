ac9c013cbd29573b0c380d09bfa10bfa02a54378
debysdm <debysdm@hotmail.com>
2022-06-23 19:23:36 -0300

    Projeto Desenvolvendo um Jogo da Memoria

---
+++ Bootcamps/TQI Fullstack Developer/Modulo III/10 - DESAFIO TQI - JAVASCRIPT/desafio3.js
@@ -0,0 +1,13 @@
+let imput = gets()
+
+let qtdEntradas = parseInt(imput);
+let melhor = 999;
+
+for(let i = 0, i < qtdEntradas; i++) {
+    imput - gets()
+    let tempo = parseFloat(imput);
+    if(tempo < melhor) {
+        melhor = tempo};
+}
+
+print(melhor)
---
+++ Bootcamps/TQI Fullstack Developer/Modulo III/10 - DESAFIO TQI - JAVASCRIPT/desafio4.js
@@ -0,0 +1,8 @@
+var array = [ 2, 3, 5, 7, 11, 13, 18, 34 ];
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+ 
+for (let i = 0; i < array.length; i++){
+    if (array[i] % 2 == 0) {
+        print(array[i]);
+     }          
